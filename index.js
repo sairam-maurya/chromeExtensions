886f7bdb9e5d0bda75a4aa360cafe15c9b42afcc
sairam-maurya <rams44258@gmail.com>
2023-06-25 10:35:57 -1200

    final commit

--- index.js
+++ index.js
@@ -4,40 +4,31 @@ const inputEl = document.getElementById("input-el")
 
 const ulEl = document.getElementById("ul-el")
 
-const deleteBtn=document.getElementById("del-btn")
+const deleteBtn = document.getElementById("del-btn")
+
+
+const tabBtb = document.getElementById("tab-btn")
+
 
 
 const inputBtn = document.getElementById("input-btn")
 
 const leadsFromsLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-if(leadsFromsLocalStorage){
-    myLeads=leadsFromsLocalStorage
-    renderLeads()
+if (leadsFromsLocalStorage) {
+    myLeads = leadsFromsLocalStorage
+    render(myLeads)
 }
 
-console.log(leadsFromsLocalStorage)
 
-
-inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-
-
-    localStorage.setItem("myLeads",JSON.stringify(myLeads))
-    renderLeads()
-
-})
-
-
-function renderLeads() {
+function render(leads) {
 
     let listItems = ""
 
 
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `<li>
-         <a href='${myLeads[i]}' target='_blank'> ${myLeads[i]}</a>
+         <a href='${leads[i]}' target='_blank'> ${leads[i]}</a>
          </li>`
 
     }
@@ -46,10 +37,47 @@ function renderLeads() {
 
 }
 
-deleteBtn.addEventListener("dblclick", function(){
+
+
+
+
+
+
+
+tabBtb.addEventListener("click", function () {
+    brower.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+
+        console.log(tabs)
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+
+
+
+
+})
+
+deleteBtn.addEventListener("dblclick", function () {
     localStorage.clear()
     ulEl.textContent = ""
 })
+
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+
+})
+
+
+
+
+
 
 
 
