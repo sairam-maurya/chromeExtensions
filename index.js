6ae78973145e641b4c4394342482dde258772da9
sairam-maurya <rams44258@gmail.com>
2023-06-24 13:35:15 -1200

    ..

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-let myLeads = [];
+let myLeads = []
+
 let inputEl = document.getElementById("input-el")
 
-const ulEl =document.getElementById("ul-el")
+const ulEl = document.getElementById("ul-el")
 
 
 let inputBtn = document.getElementById("input-btn")
 
+let leadsFromsLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+console.log(leadsFromsLocalStorage)
+
+
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
-    inputEl.value=""
+    inputEl.value = ""
+
+
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
     renderLeads()
 
 })
 
 
-function renderLeads(){
+function renderLeads() {
 
-    let listItems=""
-    
-    
-    for( let i=0; i< myLeads.length; i++){
-        listItems +="<li>"+ myLeads[i]+"</li>"
-    
-       }
-       ulEl.innerHTML=listItems
+    let listItems = ""
+
+
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `<li>
+         <a href='${myLeads[i]}' target='_blank'> ${myLeads[i]}</a>
+         </li>`
+
+    }
+    ulEl.innerHTML = listItems
 
 
 }
